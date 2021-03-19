00d4b8a6bed7227950c7b87f8e1642605d9a5ede
Fran Dios <frankdiox@gmail.com>
2021-03-19 22:02:18 +0900

    Update dependencies

--- scripts/move-dist.sh
+++ scripts/move-dist.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 cp -r dist/client serverless/public
-rm serverless/public/index.html
 mkdir serverless/api/renderer
 mv serverless/public/ssr-manifest.json serverless/api/renderer/
 cp dist/server/main.js serverless/api/renderer/
